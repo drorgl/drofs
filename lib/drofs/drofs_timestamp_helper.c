06da8909a119802aae3343ceb0a4c4da63758263
Dror Gluska <drorgl@yahoo.com>
2025-10-15 12:53:48 +0300

    cast to time_t before converting to localtime

--- lib/drofs/drofs_timestamp_helper.c
+++ lib/drofs/drofs_timestamp_helper.c
@@ -8,7 +8,8 @@ void format_timestamp(uint32_t timestamp, char *buffer, size_t buffer_size) {
     struct tm *tm_info;
 
     // Convert the timestamp to a struct tm
-    tm_info = localtime((time_t *)&timestamp);
+    time_t t = (time_t)timestamp; 
+    tm_info = localtime(&t);
 
     // Format the time as a string
     strftime(buffer, buffer_size, "%Y-%m-%d %H:%M:%S", tm_info);
